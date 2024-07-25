d2c6f8f68b05d0c098418fdd95c96d422117abc1
Shaun <lecoffeestain@gmail.com>
2024-07-25 22:42:36 +0100

    Fixed S3 sign util

--- app/lib/s3.js
+++ app/lib/s3.js
@@ -1,12 +1,12 @@
 const { S3Client } = require('@aws-sdk/client-s3');
 
 const s3 = new S3Client({
-    region: process.env.S3_REGION,
     credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY_ID,
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
     },
     endpoint: process.env.S3_ENDPOINT,
+    region: process.env.S3_REGION,
 });
 
 module.exports = s3;
--- app/utils/sign.js
+++ app/utils/sign.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
-const s3 = require('./lib/s3');
+
+console.log(process.env.S3_REGION);
+
+const s3 = require('../lib/s3');
+const { GetObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
 const key = process.argv[2];
 
-const signedUrl = getSignedUrl(s3, {
-    command: 'getObject',
-    params: {
-        Bucket: process.env.S3_BUCKET,
-        Key: key,
-    },
-    expiresIn: 3600,
+const command = new GetObjectCommand({
+    Bucket: process.env.S3_BUCKET,
+    Key: key,
 });
 
-console.log(signedUrl);
+getSignedUrl(s3, command).then(console.log).catch(console.error);
