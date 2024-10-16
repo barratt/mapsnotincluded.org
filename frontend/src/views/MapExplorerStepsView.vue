e3ee2c8dbccaa53d61230585c18d99a0b7c11754
Shaun <lecoffeestain@gmail.com>
2024-10-16 14:19:36 +0100

    Address scrollbar issue in #22

--- frontend/src/views/MapExplorerStepsView.vue
+++ frontend/src/views/MapExplorerStepsView.vue
@@ -1,8 +1,32 @@
-
 <template>
-  <iframe src="https://stefan-oltmann.de/oni-seed-browser" frameborder="0" class="w-100 " style="height: 100vh; width: 100vw"></iframe>
+  <div class="iframe-container">
+    <iframe src="https://stefan-oltmann.de/oni-seed-browser" frameborder="0"></iframe>
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: 'IframePage',
+};
 </script>
+
+<style scoped>
+main {
+  overflow: hidden;
+  height: 100vh; 
+}
+
+.iframe-container {
+  display: flex;
+  width: 100%; 
+  height: calc(100vh - 80px); /* not ideal, but I can't seem to work out how to get the frame to take the remaining space and ignore the navbar*/
+  overflow: hidden; 
+}
+
+iframe {
+  width: 100%; 
+  height: 100%;
+  border: none;
+  overflow: auto;
+}
+</style>
