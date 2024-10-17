aeece6b9189a814ba0878aa240d8677d692fd9f9
Sgt_Imalas <bene.schlai@gmx.de>
2024-10-17 20:06:53 +0200

    Mod: normalizing paths

--- mod/_WorldGenStateCapture/Paths.cs
+++ mod/_WorldGenStateCapture/Paths.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +8,9 @@ namespace _WorldGenStateCapture
 {
 	internal class Paths
 	{
-		public static string GameFolder => System.IO.Path.GetDirectoryName(UnityEngine.Application.dataPath);
-		public static string ModsFolder => System.IO.Directory.GetParent(System.IO.Directory.GetParent(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)).FullName).ToString() + "\\";
-		public static string ConfigFolder => System.IO.Path.Combine(ModsFolder, "config");
-		public static string WorldsFolder => System.IO.Path.Combine(Paths.ConfigFolder, "OfflineWorlds");
+		public static string GameFolder => Path.GetDirectoryName(UnityEngine.Application.dataPath);
+		public static string ModsFolder => Path.GetFullPath(Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)).FullName).ToString());
+		public static string ConfigFolder => Path.Combine(ModsFolder, "config");
+		public static string WorldsFolder => Path.Combine(Paths.ConfigFolder, "OfflineWorlds");
 	}
 }
