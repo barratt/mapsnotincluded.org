4a99d2ad505e27a58e66e85e4f8aa7a10c805397
Andrew Haas <ahaas@micds.org>
2024-10-18 10:30:50 -0500

    fixed text

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -13,13 +13,13 @@
                         <router-link to="/map-explorer" class="nav-link">Map Explorer</router-link>
                     </li>
                     <li class="nav-item">
+                        <router-link to="/seed-explorer" class="nav-link">Seed Explorer</router-link>
+                    </li>
+                    <li class="nav-item">
                         <router-link to="/trait-finder" class="nav-link">World Trait Finder</router-link>
                     </li>
                     <li class="nav-item">
                         <router-link to="/starmap-generator" class="nav-link">StarMap Generator</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/seed-explorer" class="nav-link">StarMap Generator</router-link>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
