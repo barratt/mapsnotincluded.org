2ff43bff0c3b451c4419830b2435ab26ca358445
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-19 01:40:09 +0200

    StarmapGeneratorView.vue: Better wording

--- frontend/src/views/StarmapGeneratorView.vue
+++ frontend/src/views/StarmapGeneratorView.vue
@@ -5,10 +5,8 @@
 <template>
   <main class="container mt-5">
     <h1>Starmap Generator</h1>
-    <p>Unfortunatly, the starmap generator is still a work in progress.</p>
-    <p>When the starmap generator is working, it will be put here.</p>
-        
+    <p>A starmap generator will be available here once development is complete.</p>
     <br>
-    <p>You can also <router-link to="/contribute">contribute</router-link> to the development of this website.</p>
+    <p>If you'd like to get involved, feel free to <router-link to="/contribute">contribute</router-link> to the development of this site.</p>
   </main>
 </template>
