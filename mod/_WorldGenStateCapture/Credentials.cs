bbfadd6ec42eb37941855b98c70e1cff918789f8
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-21 16:45:29 +0200

    Credentials.cs: Endpoint is now ingest.mapsnotincluded.org

--- mod/_WorldGenStateCapture/Credentials.cs
+++ mod/_WorldGenStateCapture/Credentials.cs
@@ -9,7 +9,7 @@ namespace _WorldGenStateCapture
 	internal class Credentials
 	{
 		public static string API_Key = "7FE9SFM9jdv7CcL";
-		public static string API_URL_UPLOAD = "https://apibeta.mapsnotincluded.org/upload";
-		public static string API_URL_REPORT_FAILED = "https://apibeta.mapsnotincluded.org/report-worldgen-failure";
+		public static string API_URL_UPLOAD = "https://ingest.mapsnotincluded.org/upload";
+		public static string API_URL_REPORT_FAILED = "https://ingest.mapsnotincluded.org/report-worldgen-failure";
 	}
 }
