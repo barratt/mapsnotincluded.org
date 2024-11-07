e287fb31daa396eadaa30323cd25d222c239a7b1
SGT_Imalas <bene.schlai@gmx.de>
2024-11-07 23:43:22 +0100

    Regex implementation for Seed Request (#71)

---
+++ frontend/src/content/zh-CN/components/coordinate_request_dialog.js
@@ -0,0 +1,7 @@
+export default {
+  request_coordinate_title: "Request new Seed",
+  request_coordinate_text: "Request a seed thats not in the database yet",
+  request_coordinate_invalid_syntax: "Not a valid seed!",
+  request_coordinate_already_in_db: "Seed is already in database!",
+  request_coordinate_view_seed: "View in Map Explorer",
+};
