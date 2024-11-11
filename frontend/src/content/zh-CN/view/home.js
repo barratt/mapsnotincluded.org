eadeef0cdd777baa59bf60c4f28ffc961ee39bbc
jason21415 <jason83228@gmail.com>
2024-11-11 17:10:55 +0800

    Chinese translation update
    Chinese translation update

--- frontend/src/content/zh-CN/view/home.js
+++ frontend/src/content/zh-CN/view/home.js
@@ -1,7 +1,7 @@
 export default {
   "title": "欢迎来到 MapsNotIncluded!",
-  "description_1": "该网站目前正在开发中,最终将包含有关缺氧的各种工具和资源.我们的地图种子浏览器已经上线,在您的帮助下,我们很快就会添加更多有用的功能.",
-  "description_2": "想参与吗？前往我们的{contributeLink}页面.您也可以加入我们的 {discordLink} 群组或透过 {githubLink} 提交问题和拉取请求!",
+  "description_1": "该网站目前正在开发中,最终将包含有关缺氧的各种工具和资源. 我们的地图种子浏览器已经上线,在您的帮助下,我们很快就会添加更多有用的功能.",
+  "description_2": "想参与吗？前往我们的{contributeLink}页面. 您也可以加入我们的 {discordLink} 群组或透过 {githubLink} 提交问题和拉取请求!",
   "link_map_explorer": "地图种子浏览器",
   "link_trait_finder": "星球特质查找器",
   "link_starmap_generator": "星图产生器",
--- frontend/src/content/zh-TW/view/not_found.js
+++ frontend/src/content/zh-TW/view/not_found.js
@@ -1,5 +1,5 @@
 export default {
   "title": `404 找不到網頁`,
   "description_1": `抱歉,您要尋找的頁面不存在.`,
-  "description_2": `如果您透過點擊來訪問此頁面,則很可能它尚未完成構建,如果您有技術與頭腦,請使用我們的 {githubLink} 進行幫助,或是來我們的 {discordLink} 群組找我們.`
+  "description_2": `如果您透過點擊來訪問此頁面,則很可能它尚未完成構建. 如果您有技術與頭腦,請使用我們的 {githubLink} 進行幫助,或是來我們的 {discordLink} 群組找我們.`
 }
