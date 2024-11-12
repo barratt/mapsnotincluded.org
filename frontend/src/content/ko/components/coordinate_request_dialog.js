de15bbc75f55d7d6492877ab1d0171ab2acc2114
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2024-11-12 21:21:08 +0900

    feat: shortened key names

--- frontend/src/content/ko/components/coordinate_request_dialog.js
+++ frontend/src/content/ko/components/coordinate_request_dialog.js
@@ -1,7 +1,7 @@
 export default {
-  request_coordinate_title: "시드 요청",
-  request_coordinate_text: "아직 데이터베이스에 없는 시드를 입력해 주세요",
-  request_coordinate_invalid_syntax: "유효한 시드가 아닙니다",
-  request_coordinate_already_in_db: "시드가 이미 데이터베이스에 있습니다",
-  request_coordinate_view_seed: "지도 탐색기에서 보기",
+  title: "시드 요청",
+  text: "아직 데이터베이스에 없는 시드를 입력해 주세요",
+  invalid_syntax: "유효한 시드가 아닙니다",
+  already_in_db: "시드가 이미 데이터베이스에 있습니다",
+  view_seed: "지도 탐색기에서 보기",
 };
