5f139b2ca0c5ebb433eed9e9adc0739917b3c635
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2024-11-12 21:14:48 +0900

    fix: fixed translation import

--- frontend/src/content/ko/components/coordinate_request_dialog.js
+++ frontend/src/content/ko/components/coordinate_request_dialog.js
@@ -1,5 +1,5 @@
 export default {
-  request_coordinate_title: "요청 좌표",
+  request_coordinate_title: "시드 요청",
   request_coordinate_text: "아직 데이터베이스에 없는 시드를 입력해 주세요",
   request_coordinate_invalid_syntax: "유효한 시드가 아닙니다",
   request_coordinate_already_in_db: "시드가 이미 데이터베이스에 있습니다",
