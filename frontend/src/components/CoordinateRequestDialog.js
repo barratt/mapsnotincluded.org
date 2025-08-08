16a2367bf62b2c5095bc466a9f3ab2c04d6bbd14
Stefan Oltmann <github@stefan-oltmann.de>
2025-08-08 20:48:15 +0200

    Try to repair coordinate request feature

--- frontend/src/components/CoordinateRequestDialog.js
+++ frontend/src/components/CoordinateRequestDialog.js
@@ -1,80 +1,81 @@
 import Swal from "sweetalert2";
-import { useUserStore } from "@/stores";
+import {useUserStore} from "@/stores";
 import i18n from "@/i18n";
-const { t } = i18n.global;
+
+const {t} = i18n.global;
 
 export function requestCoordinate() {
-  const apiUrl = import.meta.env.VITE_API_URL;
-  // Lets pop up a swal to ask for the seed name and then send it to the backend
-  Swal.fire({
-    title: t("coordinate_request_dialog.text"),
-    input: "text",
-    inputLabel: t("coordinate_request_dialog.coordinate"),
-    inputPlaceholder: t("coordinate_request_dialog.placeholder"),
-    showCancelButton: true,
-    confirmButtonText: t("coordinate_request_dialog.bt_request"),
-    cancelButtonText: t("coordinate_request_dialog.bt_cancel"),
-    showLoaderOnConfirm: true,
-    inputValidator: (coordinate) => validateCoordinate(coordinate),
-    preConfirm: (coordinates) => {
 
-      const userStore = useUserStore();
-      const token = userStore.getValidToken();
+    Swal.fire({
+        title: t("coordinate_request_dialog.text"),
+        input: "text",
+        inputLabel: t("coordinate_request_dialog.coordinate"),
+        inputPlaceholder: t("coordinate_request_dialog.placeholder"),
+        showCancelButton: true,
+        confirmButtonText: t("coordinate_request_dialog.bt_request"),
+        cancelButtonText: t("coordinate_request_dialog.bt_cancel"),
+        showLoaderOnConfirm: true,
+        inputValidator: (coordinate) => validateCoordinate(coordinate),
+        preConfirm: (coordinates) => {
 
-      if (!token) {
-        throw new Error("Authentication required. Please login.");
-      }
+            const userStore = useUserStore();
+            const token = userStore.getValidToken();
 
-      return fetch(`${apiUrl}/coordinates/request/` + coordinates, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => {
-          if (!response.ok) {
-            if (response.status === 401) {
-              useUserStore().clearToken();
-              throw new Error("Unauthorized, please login again!");
+            if (!token) {
+                throw new Error("Authentication required. Please login.");
             }
-          }
 
-          return response.json();
-        })
-        .then((data) => {
-          if (data.error) {
-            throw new Error(data.error);
-          }
-          return data;
-        })
+            return fetch(`https://ingest.mapsnotincluded.org/request-coordinate`, {
+                method: "POST",
+                body: coordinates,
+                headers: {
+                    "token": token
+                },
+            })
+                .then((response) => {
+                    if (!response.ok) {
+                        if (response.status === 401) {
+                            useUserStore().clearToken();
+                            throw new Error("Unauthorized, please login again!");
+                        }
+                    }
 
-        .catch((error) => {
-          Swal.showValidationMessage(`${error}`);
-        });
-    },
-    allowOutsideClick: () => !Swal.isLoading(),
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: t("coordinate_request_dialog.request_successful"),
-        text: result.value.message,
-        icon: "success",
-      });
-    }
-  });
+                    return response.json();
+                })
+                .then((data) => {
+                    if (data.error) {
+                        throw new Error(data.error);
+                    }
+                    return data;
+                })
+
+                .catch((error) => {
+                    Swal.showValidationMessage(`${error}`);
+                });
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: t("coordinate_request_dialog.request_successful"),
+                text: result.value.message,
+                icon: "success",
+            });
+        }
+    });
 }
 
 export function validateCoordinate(coordinate) {
-  const coordinateValidationRegex = RegExp(
-    /(^(SNDST-A|CER-A|CERS-A|PRE-A|PRES-A|OCAN-A|S-FRZ|LUSH-A|FRST-A|VOLCA|BAD-A|HTFST-A|OASIS-A|V-SNDST-C|V-CER-C|V-CERS-C|V-PRE-C|V-PRES-C|V-OCAN-C|V-SWMP-C|V-SFRZ-C|V-LUSH-C|V-FRST-C|V-VOLCA-C|V-BAD-C|V-HTFST-C|V-OASIS-C|SNDST-C|CER-C|PRE-C|FRST-C|SWMP-C|M-SWMP-C|M-BAD-C|M-FRZ-C|M-FLIP-C|M-RAD-C|M-CERS-C)-\d+-[^-]*-[^-]*-[^-]*)/gm
-  );
-  return new Promise((resolve) => {
-    console.log(coordinate);
-    let coordinateValid = coordinateValidationRegex.test(coordinate);
-    if (!coordinateValid) {
-      resolve(t("coordinate_request_dialog.invalid_syntax"));
-    } else {
-      resolve();
-    }
-  });
+    const coordinateValidationRegex = RegExp(
+        /(^(SNDST-A|CER-A|CERS-A|PRE-A|PRES-A|OCAN-A|S-FRZ|LUSH-A|FRST-A|VOLCA|BAD-A|HTFST-A|OASIS-A|V-SNDST-C|V-CER-C|V-CERS-C|V-PRE-C|V-PRES-C|V-OCAN-C|V-SWMP-C|V-SFRZ-C|V-LUSH-C|V-FRST-C|V-VOLCA-C|V-BAD-C|V-HTFST-C|V-OASIS-C|SNDST-C|CER-C|PRE-C|FRST-C|SWMP-C|M-SWMP-C|M-BAD-C|M-FRZ-C|M-FLIP-C|M-RAD-C|M-CERS-C)-\d+-[^-]*-[^-]*-[^-]*)/gm
+    );
+    return new Promise((resolve) => {
+        console.log(coordinate);
+        let coordinateValid = coordinateValidationRegex.test(coordinate);
+        if (!coordinateValid) {
+            resolve(t("coordinate_request_dialog.invalid_syntax"));
+        } else {
+            resolve();
+        }
+    });
 }
