7400a93fda8cf4bd72e312455b5060db14dbc626
barratt <lecoffeestain@gmail.com>
2026-02-07 20:29:44 +0000

    Fix build: remove missing privacy route, fix logout script setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,48 +14,43 @@ body {
 </style>
 
 <script setup>
+import { onMounted, onBeforeUnmount } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
 import { RouterView } from 'vue-router'
-</script>
+import { useUserStore } from './stores'
 
-<script>
-import { useUserStore } from './stores';
+const router = useRouter()
+const route = useRoute()
+const userStore = useUserStore()
 
-
-export default {
-  data() { 
-    return {};
-  },
-  methods: {
-    handleLogoutHash() {
-      if (window.location.hash === '#logout') {
-        useUserStore().clearToken();
-        const url = new URL(window.location.href);
-        url.hash = '';
-        window.history.replaceState({}, '', url);
-        return true;
-      }
-      return false;
-    }
-  },
-  mounted() {
-    console.log("App mounted");
-
-    this.$router.isReady().then(() => {
-      if (this.handleLogoutHash()) {
-        return;
-      }
-      const token = this.$route.query.token;
-      if (token) {
-        useUserStore().setToken(token);
-        this.$router.replace({ query: {} });
-      }
-    });
-
-    window.addEventListener('hashchange', this.handleLogoutHash);
-  },
-  beforeUnmount() {
-    window.removeEventListener('hashchange', this.handleLogoutHash);
+function handleLogoutHash() {
+  if (window.location.hash === '#logout') {
+    userStore.clearToken()
+    const url = new URL(window.location.href)
+    url.hash = ''
+    window.history.replaceState({}, '', url)
+    return true
   }
+  return false
 }
+
+onMounted(() => {
+  router.isReady().then(() => {
+    if (handleLogoutHash()) {
+      return
+    }
+    const token = route.query.token
+    if (token) {
+      userStore.setToken(token)
+      router.replace({ query: {} })
+    }
+  })
+
+  window.addEventListener('hashchange', handleLogoutHash)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('hashchange', handleLogoutHash)
+})
 </script>
